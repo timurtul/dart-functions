e6225747b17ebdfd8ebb865e6c22bd8e95173ebe
Timur Tül <38362206+timurtul@users.noreply.github.com>
2022-02-19 17:46:09 +0300

    Update functions.dart

--- functions.dart
+++ functions.dart
@@ -1,13 +1,13 @@
 //default function
 
 defaultFunc([String name = "Timur"]){
-  
+  return print(' Adım $name ');
 }
 
 //named func
 
 namedFunc({String ad = "Timur"}){
-  
+  return print ('Adım $ad');
 }
 
 //required func
