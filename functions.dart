3d69ed92bb3650fc21de2a88be9bcadbb6d59081
Timur Tül <38362206+timurtul@users.noreply.github.com>
2022-02-19 17:43:29 +0300

    Create functions.dart

---
+++ functions.dart
@@ -0,0 +1,21 @@
+//default function
+
+defaultFunc([String name = "Timur"]){
+  
+}
+
+//named func
+
+namedFunc({String ad = "Timur"}){
+  
+}
+
+//required func
+
+reqFunc({required int yas}){
+  
+}
+
+void main(){
+  
+}
